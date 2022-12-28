132accad7b1c1f2d330472dfdb7ad0207b1190e9
JallaJaswanth <jallajaswanth@gmail.com>
2022-12-28 19:55:34 +0530

    fixed register error

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,58 +1,69 @@
-import React from 'react'
-import { Row, Col, Form, Input, Checkbox } from 'antd';
-import { Link } from "react-router-dom"
-import { useDispatch, useSelector } from 'react-redux';
-import { userLogin } from '../redux/actions/UserActions';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-import Spinner from '../components/Spinner';
+import React from "react";
+import { Row, Col, Form, Input, Checkbox } from "antd";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "../redux/actions/UserActions";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+import Spinner from "../components/Spinner";
 // ..
 AOS.init();
 
-
 function Login() {
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.alertsReducer);
+  function onFinish(values) {
+    dispatch(userLogin(values));
+    console.log(values);
+  }
 
-    const dispatch = useDispatch()
-    const { loading } = useSelector(state => state.alertsReducer)
-    function onFinish(values) {
-        dispatch(userLogin(values))
-        console.log(values)
-    }
+  return (
+    <div className="login d-flex align-items-center">
+      {loading && <Spinner />}
+      <Row gutter={16}>
+        <Col lg={16} style={{ position: "relative" }}>
+          <img
+            data-aos="slide-right"
+            data-aos-duration="1500"
+            className="imgLogin"
+            src="https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80"
+          />
+          {/* <h1 className='loginLogo'>Car Rental</h1> */}
+        </Col>
+        <Col lg={8} className="text-left p-5">
+          <Form
+            layout="vertical"
+            className="loginForm p-5 mt-2"
+            onFinish={onFinish}
+          >
+            <h1>Login</h1>
+            <hr />
+            <Form.Item
+              name="username"
+              label="Username"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
 
-    return (
-        <div className='login d-flex align-items-center'>
-            {loading && (<Spinner />)}
-            <Row gutter={16}>
-                <Col lg={16} style={{ position: 'relative' }}>
-                    <img
-                        data-aos='slide-right'
-                        data-aos-duration='1500'
-                        className='imgLogin' src='https://images.unsplash.com/photo-1485291571150-772bcfc10da5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1856&q=80' />
-                    {/* <h1 className='loginLogo'>Car Rental</h1> */}
-                </Col>
-                <Col lg={8} className='text-left p-5' >
-                    <Form layout='vertical' className='loginForm p-5 mt-2' onFinish={onFinish}>
-                        <h1>Login</h1>
-                        <hr />
-                        <Form.Item name='username' label='Username' rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
 
-                        <Form.Item name='password' label='Password' rules={[{ required: true }]}>
-                            <Input />
-                        </Form.Item>
+            <button className="btn1">Login</button>
 
-                        <button className='btn1'>Login</button>
-
-                        <Link to='/Register' className='d-flex mt-3'>Click Here to Register</Link>
-
-
-
-                    </Form>
-                </Col>
-            </Row>
-        </div>
-    )
+            <Link to="/Register" className="d-flex mt-3">
+              Click Here to Register
+            </Link>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  isAdmin: { type: String, required: true },
+  isAdmin: { type: String },
 });
 
-const userModel = mongoose.model('users', userSchema);
+const userModel = mongoose.model("users", userSchema);
 
 module.exports = userModel;
